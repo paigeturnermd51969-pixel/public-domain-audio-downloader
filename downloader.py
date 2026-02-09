7dd3a7581b224130a7260cf4341f8bda068389d3
Paige Turner <paigeturnermd51969@gmail.com>
2026-02-08 20:55:17 -0800

    Basic POC frontend

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+#from argparse import ArgumentParser
 from pathlib import Path
 import sys
 import os
@@ -7,60 +7,64 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from yt_dlp import YoutubeDL
 
-parser = ArgumentParser()
-parser.add_argument("playlist_link", type=str, help="link to the album playlist (must be wraped in quotes)")
-args = parser.parse_args()
-if args.playlist_link == "":
-    print("Usage: python3 downloader.py \"<link-to-album-playlist>\"")
-    print("Note: Quotes are required around the link")
-    sys.exit()
+#parser = ArgumentParser()
+#parser.add_argument("playlist_link", type=str, help="link to the album playlist (must be wraped in quotes)")
+#args = parser.parse_args()
+#if args.playlist_link == "":
+#    print("Usage: python3 downloader.py \"<link-to-album-playlist>\"")
+#    print("Note: Quotes are required around the link")
+#    sys.exit()
 
-driver = webdriver.Chrome()
-driver.get(args.playlist_link)
+#def download_song(url: str):
 
-driver.implicitly_wait(5)
-playlist = driver.find_element(By.ID, "playlist")
-videos = playlist.find_element(By.ID, "items")
-link_elements = videos.find_elements(By.ID, "wc-endpoint")
-video_title_elements = videos.find_elements(By.ID, "video-title")
+def download(url: str):
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-video_titles = []
-for t in video_title_elements:
-    video_titles.append(t.get_attribute("title").replace("/", "_slash_"))
-    video_titles.append(t.get_attribute("title").replace("\\", "_back_slash_"))
+    driver.implicitly_wait(5)
+    playlist = driver.find_element(By.ID, "playlist")
+    videos = playlist.find_element(By.ID, "items")
+    link_elements = videos.find_elements(By.ID, "wc-endpoint")
+    video_title_elements = videos.find_elements(By.ID, "video-title")
 
-links = []
-for l in link_elements:
-    links.append(l.get_attribute("href"))
+    video_titles = []
+    for t in video_title_elements:
+        video_titles.append(t.get_attribute("title").replace("/", "_slash_"))
+        video_titles.append(t.get_attribute("title").replace("\\", "_back_slash_"))
 
-title_element = driver.find_element(By.ID, "header-description")
-title = title_element.find_element(By.TAG_NAME, "yt-formatted-string").get_attribute("title")
-title = title.replace("/", "_slash_")
-title = title.replace("\\", "_back_slash_")
-Path(title).mkdir(exist_ok=True)
+    links = []
+    for l in link_elements:
+        links.append(l.get_attribute("href"))
 
-ydl_opts = {
-    #"format": "bestaudio", # This might be something that's deprecated (not sure)
-    "format": "m4a/bestaudio/best",
-    "postprocessors": [{
-        "key": "FFmpegExtractAudio",
-        "preferredcodec": "opus",
-    }],
-    "noplaylist": True,
-    #"outtmpl": title+"/%(title)s",
-}
+    title_element = driver.find_element(By.ID, "header-description")
+    title = title_element.find_element(By.TAG_NAME, "yt-formatted-string").get_attribute("title")
+    title = title.replace("/", "_slash_")
+    title = title.replace("\\", "_back_slash_")
+    Path(title).mkdir(exist_ok=True)
 
-dir_sep = "/"
-if os.name == "nt":
-    dir_sep = "\\"
+    ydl_opts = {
+        #"format": "bestaudio", # This might be something that's deprecated (not sure)
+        "format": "m4a/bestaudio/best",
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "opus",
+        }],
+        "noplaylist": True,
+        #"outtmpl": title+"/%(title)s",
+    }
 
-for i in range(len(links)):
-    prefix = ""
-    if i < 9:
-        prefix = "0"+str(i+1)+" - "
-    else:
-        prefix = str(i+1)+" - "
-    
-    ydl_opts["outtmpl"] = title+dir_sep+prefix+video_titles[i]
-    with YoutubeDL(ydl_opts) as ydl:
-        error_code = ydl.download(links[i])
+    dir_sep = "/"
+    if os.name == "nt":
+        dir_sep = "\\"
+
+    for i in range(len(links)):
+        prefix = ""
+        if i < 9:
+            prefix = "0"+str(i+1)+" - "
+        else:
+            prefix = str(i+1)+" - "
+        
+        ydl_opts["outtmpl"] = title+dir_sep+prefix+video_titles[i]
+        with YoutubeDL(ydl_opts) as ydl:
+            error_code = ydl.download(links[i])
+
---
+++ frontend.py
@@ -0,0 +1,71 @@
+import sys
+from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog, QTextEdit, QWidget)
+from PySide6.QtGui import (QCloseEvent, QTextCursor)
+from PySide6.QtCore import (QObject, Signal)
+from downloader import download
+
+class StdOutWindow(QWidget):
+    def __init__(self):
+        super(StdOutWindow, self).__init__()
+        layout = QVBoxLayout()
+        self.setWindowTitle('Loading Data')
+        self.txt_edit = QTextEdit()
+        layout.addWidget(self.txt_edit)
+        self.setLayout(layout)
+        self.setMinimumSize(600, 300)
+
+        sys.stdout = EmittingStream()
+        sys.stdout.text_written.connect(self.normal_output_written)
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        sys.stdout = sys.__stdout__
+        event.accept()
+
+    def normal_output_written(self, text):
+        cursor = self.txt_edit.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.txt_edit.setTextCursor(cursor)
+        self.txt_edit.ensureCursorVisible()
+
+class EmittingStream(QObject):
+    text_written = Signal(str)
+
+    def write(self, text):
+        self.text_written.emit(str(text))
+
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit()
+        self.button = QPushButton("Download")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+        # Window options
+        self.setWindowTitle("Public Domain Audio Downloader")
+
+    # Greets the user
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
+        download(self.edit.text())
+    
+    
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    #stdOutWindow = StdOutWindow()
+    #stdOutWindow.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
